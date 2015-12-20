51b0dd70ef317b62786407987ea0253b68f4bb0a
Theo <acwazytomato@gmail.com>
2015-12-19 17:24:28 -0800

    small edit

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,7 +3,7 @@ class Api::ArticlesController < ApplicationController
   def create
     # saves an article to the specific user, no annotations yet.
     url = article_params[:url]
-    id = article_params[:user_id]
+    id = article_params[:user_id].to_i
     # user = User.find_by_session_token(article_params[:session_token])
     @article = Article.new({url: url, user_id: id})
     if @article.save
