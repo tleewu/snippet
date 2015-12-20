d9b93afae1dc7a419be3b4dacb7803d74a8cf4cb
Theo <acwazytomato@gmail.com>
2015-12-19 16:54:27 -0800

    testing postman

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,8 +3,9 @@ class Api::ArticlesController < ApplicationController
   def create
     # saves an article to the specific user, no annotations yet.
     url = article_params[:url]
-    user = User.find_by_session_token(article_params[:session_token])
-    @article = Article.new({url: url, user_id: user.id})
+    id = article_params[:user_id]
+    # user = User.find_by_session_token(article_params[:session_token])
+    @article = Article.new({url: url, user_id: id})
     if @article.save
       render json: @article.to_json
     else
@@ -16,6 +17,6 @@ class Api::ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:session_token, :url)
+    params.require(:article).permit(:url, :user_id)
   end
 end
