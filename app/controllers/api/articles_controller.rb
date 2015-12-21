af67666b471242e65a40742a7d55399840b1a4bd
Theo <acwazytomato@gmail.com>
2015-12-20 16:42:41 -0800

    testing out cookies feature

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,9 +3,8 @@ class Api::ArticlesController < ApplicationController
   def create
     # saves an article to the specific user, no annotations yet.
     url = params[:url]
-    id = params[:user_id].to_i
     # user = User.find_by_session_token(article_params[:session_token])
-    @article = Article.new({url: url, user_id: id})
+    @article = Article.new({url: url, user_id: current_user.id})
     if @article.save
       render json: @article.to_json
     else
