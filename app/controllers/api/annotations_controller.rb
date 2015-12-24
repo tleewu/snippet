d810ddc17945d320660c849f0631f97cabdb97ef
Theo <acwazytomato@gmail.com>
2015-12-23 21:55:13 -0800

    add ability to create annotations

---
+++ app/controllers/api/annotations_controller.rb
@@ -0,0 +1,23 @@
+class Api::AnnotationsController < ApplicationController
+
+  def create
+    user_id = @current_user.id
+    current_article = Article.find_by_url_and_user_id({url: annotation_params[:url], user_id: user_id})
+    unless current_article
+      current_article = Article.create!({user_id: user_id, url: url})
+    end
+    annotation_params[:article_id] = current_article.id
+    @annotation = Annotation.new(annotation_params)
+    if @annotation.save
+      render json: @annotation.to_json
+    else
+      render json: {errors: @annotation.errors.full_messages}
+    end
+  end
+
+  private
+
+  def annotation_params
+    params.require(:annotation).permit(:url, :highlight, :start_node, :start_offset)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :articles, only: [:create]
+    resources :annotations, only: [:create]
     # resources :annotations, only: [:create]
   end
 
